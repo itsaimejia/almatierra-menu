09ae48407bf9fe1ba69b75e5ab1bf146a44c235c
itsaimejia <33639089+itsaimejia@users.noreply.github.com>
2022-11-01 00:25:55 -0700

    id pagina menu

--- components/CardMenu.tsx
+++ components/CardMenu.tsx
@@ -2,77 +2,75 @@ import { IconArrowNarrowRight } from '@tabler/icons';
 import { Card, Text, Group, createStyles } from '@mantine/core';
 
 const useStyles = createStyles((theme, _params, getRef) => {
-  const image = getRef('image');
-  console.log(image)
+    const image = getRef('image');
+    return {
+        card: {
+            position: 'relative',
+            height: 250,
+            backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
 
-  return {
-    card: {
-      position: 'relative',
-      height: 280,
-      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+            [`&:hover .${image}`]: {
+                transform: 'scale(1.03)',
+            },
+        },
 
-      [`&:hover .${image}`]: {
-        transform: 'scale(1.03)',
-      },
-    },
+        image: {
+            ref: image,
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundSize: 'cover',
+            transition: 'transform 500ms ease',
+        },
 
-    image: {
-      ref: image,
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      right: 0,
-      bottom: 0,
-      backgroundSize: 'cover',
-      transition: 'transform 500ms ease',
-    },
+        content: {
+            height: '100%',
+            position: 'relative',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'flex-end',
+            zIndex: 1,
+        },
 
-    content: {
-      height: '100%',
-      position: 'relative',
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'flex-end',
-      zIndex: 1,
-    },
-
-    title: {
-      color: theme.white,
-      marginBottom: 5,
-    },
-  };
+        title: {
+            color: theme.white,
+            marginBottom: 5,
+        },
+    };
 });
 
 interface ImageCardProps {
 
-  image: string;
-  title: string;
+    image: string;
+    title: string;
 
 }
 
 export function CardMenu({ image, title }: ImageCardProps) {
-  const { classes, theme } = useStyles();
+    const { classes, theme } = useStyles();
 
-  return (
-    <Card
-      p="lg"
-      shadow="lg"
-      className={classes.card}
-      radius="md"
-      component="a"
-      href={'#'}
-      target="_blank"
-    >
-      <div className={classes.image} style={{ backgroundImage: `url(${image})` }} />
-      <div className={classes.content}>
+    return (
+        <Card
+            p="lg"
+            shadow="lg"
+            className={classes.card}
+            radius="md"
+            component="a"
+            href={`/menu/${title.toLocaleLowerCase()}`}
+            target="_self"
+        >
+            <div className={classes.image} style={{ backgroundImage: `url(${image})` }} />
+            <div className={classes.content}>
 
-        <Group position="apart" spacing="xs">
-          <Text size="lg" className={classes.title} weight={500}>
-            {title}
-          </Text>
-          <IconArrowNarrowRight size={18}  color= 'white' />
-        </Group>
+                <Group position="apart" spacing="xs">
+                    <Text size="lg" className={classes.title} weight={500}>
+                        {title}
+                    </Text>
+                    <IconArrowNarrowRight size={18} color='white' />
+                </Group>
 
-      </div>
-    </Card>)
+            </div>
+        </Card>)
 }
