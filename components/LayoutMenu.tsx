e6c81282f6f87562cf2962f1eaa82492102ef610
itsaimejia <33639089+itsaimejia@users.noreply.github.com>
2022-11-09 00:09:18 -0800

    base de datos Vicky

--- components/LayoutMenu.tsx
+++ components/LayoutMenu.tsx
@@ -13,6 +13,7 @@ export default function LayoutMenu({ children }: { children: any }) {
     const router = useRouter()
     /*Getting the second element of the array.*/
     const currentMenu = router.asPath.split('/')[2]
+    console.log(currentMenu)
     /**
      * It returns the first element of the dataMenus array that has a title property equal to the
      * currentMenu variable.
--- utils/data.ts
+++ utils/data.ts
@@ -36,6 +36,13 @@ export const dataMenus = [
         categories: ['Premium', 'Reserva', 'Crianza', 'Blancos', 'Rosados']
     },
 ]
+export const imageCymbals = [
+    { menu: 'Comida', categorie: 'Entradas', image: 'https://scontent.ftij2-1.fna.fbcdn.net/v/t39.30808-6/294053776_3208482269366547_6874948848904432638_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGQc1c6b_wRjxqsJCkMHVKF5kKvyS13-2XmQq_JLXf7ZVVNZg_brFoXQ-tPcqYMnFWDdk38Zamgmmh_tXBRkagm&_nc_ohc=TEO88CN1jIgAX-bOzM7&_nc_ht=scontent.ftij2-1.fna&oh=00_AfDgDAYjGtaalNHJG7_0uee0yIiMN_W0vSpI0ucOfcwppw&oe=6369E6DF' },
+    { menu: 'Comida', categorie: 'Entradas', image: 'https://scontent.ftij2-1.fna.fbcdn.net/v/t39.30808-6/294053776_3208482269366547_6874948848904432638_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGQc1c6b_wRjxqsJCkMHVKF5kKvyS13-2XmQq_JLXf7ZVVNZg_brFoXQ-tPcqYMnFWDdk38Zamgmmh_tXBRkagm&_nc_ohc=TEO88CN1jIgAX-bOzM7&_nc_ht=scontent.ftij2-1.fna&oh=00_AfDgDAYjGtaalNHJG7_0uee0yIiMN_W0vSpI0ucOfcwppw&oe=6369E6DF' },
+    { menu: 'Comida', categorie: 'Entradas', image: 'https://scontent.ftij2-1.fna.fbcdn.net/v/t39.30808-6/294053776_3208482269366547_6874948848904432638_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGQc1c6b_wRjxqsJCkMHVKF5kKvyS13-2XmQq_JLXf7ZVVNZg_brFoXQ-tPcqYMnFWDdk38Zamgmmh_tXBRkagm&_nc_ohc=TEO88CN1jIgAX-bOzM7&_nc_ht=scontent.ftij2-1.fna&oh=00_AfDgDAYjGtaalNHJG7_0uee0yIiMN_W0vSpI0ucOfcwppw&oe=6369E6DF' },
+    { menu: 'Comida', categorie: 'Entradas', image: 'https://scontent.ftij2-1.fna.fbcdn.net/v/t39.30808-6/294053776_3208482269366547_6874948848904432638_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGQc1c6b_wRjxqsJCkMHVKF5kKvyS13-2XmQq_JLXf7ZVVNZg_brFoXQ-tPcqYMnFWDdk38Zamgmmh_tXBRkagm&_nc_ohc=TEO88CN1jIgAX-bOzM7&_nc_ht=scontent.ftij2-1.fna&oh=00_AfDgDAYjGtaalNHJG7_0uee0yIiMN_W0vSpI0ucOfcwppw&oe=6369E6DF' },
+    { menu: 'Comida', categorie: 'Entradas', image: 'https://scontent.ftij2-1.fna.fbcdn.net/v/t39.30808-6/294053776_3208482269366547_6874948848904432638_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGQc1c6b_wRjxqsJCkMHVKF5kKvyS13-2XmQq_JLXf7ZVVNZg_brFoXQ-tPcqYMnFWDdk38Zamgmmh_tXBRkagm&_nc_ohc=TEO88CN1jIgAX-bOzM7&_nc_ht=scontent.ftij2-1.fna&oh=00_AfDgDAYjGtaalNHJG7_0uee0yIiMN_W0vSpI0ucOfcwppw&oe=6369E6DF' },
+]
 
 export const dataCymbals = [
     { menu: 'Comida', categorie: 'Entradas', name: 'Crudo de atun', price: '310', description: 'Con recado negro y miso' },
@@ -45,35 +52,15 @@ export const dataCymbals = [
     { menu: 'Comida', categorie: 'Entradas', name: 'Tuetanos al grill', price: '350', description: 'Cebolla curtida y aguacate' },
     { menu: 'Comida', categorie: 'Entradas', name: 'Tlacoyos de pulpo y camaron', price: '280', description: 'Pulpo y camaron al ajillo' },
     { menu: 'Comida', categorie: 'Entradas', name: 'Bao de pato en salsa agridulce', price: '250', description: 'Salsa agridulce de chile ancho,pepino y cilantro' },
-
-]
-
-export const imageCymbals = [
-    { menu: 'Comida', categorie: 'Entradas', image: 'https://scontent.ftij2-1.fna.fbcdn.net/v/t39.30808-6/294053776_3208482269366547_6874948848904432638_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGQc1c6b_wRjxqsJCkMHVKF5kKvyS13-2XmQq_JLXf7ZVVNZg_brFoXQ-tPcqYMnFWDdk38Zamgmmh_tXBRkagm&_nc_ohc=TEO88CN1jIgAX-bOzM7&_nc_ht=scontent.ftij2-1.fna&oh=00_AfDgDAYjGtaalNHJG7_0uee0yIiMN_W0vSpI0ucOfcwppw&oe=6369E6DF' },
-    { menu: 'Comida', categorie: 'Entradas', image: 'https://scontent.ftij2-1.fna.fbcdn.net/v/t39.30808-6/294053776_3208482269366547_6874948848904432638_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGQc1c6b_wRjxqsJCkMHVKF5kKvyS13-2XmQq_JLXf7ZVVNZg_brFoXQ-tPcqYMnFWDdk38Zamgmmh_tXBRkagm&_nc_ohc=TEO88CN1jIgAX-bOzM7&_nc_ht=scontent.ftij2-1.fna&oh=00_AfDgDAYjGtaalNHJG7_0uee0yIiMN_W0vSpI0ucOfcwppw&oe=6369E6DF' },
-    { menu: 'Comida', categorie: 'Entradas', image: 'https://scontent.ftij2-1.fna.fbcdn.net/v/t39.30808-6/294053776_3208482269366547_6874948848904432638_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGQc1c6b_wRjxqsJCkMHVKF5kKvyS13-2XmQq_JLXf7ZVVNZg_brFoXQ-tPcqYMnFWDdk38Zamgmmh_tXBRkagm&_nc_ohc=TEO88CN1jIgAX-bOzM7&_nc_ht=scontent.ftij2-1.fna&oh=00_AfDgDAYjGtaalNHJG7_0uee0yIiMN_W0vSpI0ucOfcwppw&oe=6369E6DF' },
-    { menu: 'Comida', categorie: 'Entradas', image: 'https://scontent.ftij2-1.fna.fbcdn.net/v/t39.30808-6/294053776_3208482269366547_6874948848904432638_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGQc1c6b_wRjxqsJCkMHVKF5kKvyS13-2XmQq_JLXf7ZVVNZg_brFoXQ-tPcqYMnFWDdk38Zamgmmh_tXBRkagm&_nc_ohc=TEO88CN1jIgAX-bOzM7&_nc_ht=scontent.ftij2-1.fna&oh=00_AfDgDAYjGtaalNHJG7_0uee0yIiMN_W0vSpI0ucOfcwppw&oe=6369E6DF' },
-    { menu: 'Comida', categorie: 'Entradas', image: 'https://scontent.ftij2-1.fna.fbcdn.net/v/t39.30808-6/294053776_3208482269366547_6874948848904432638_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGQc1c6b_wRjxqsJCkMHVKF5kKvyS13-2XmQq_JLXf7ZVVNZg_brFoXQ-tPcqYMnFWDdk38Zamgmmh_tXBRkagm&_nc_ohc=TEO88CN1jIgAX-bOzM7&_nc_ht=scontent.ftij2-1.fna&oh=00_AfDgDAYjGtaalNHJG7_0uee0yIiMN_W0vSpI0ucOfcwppw&oe=6369E6DF' },
-]
-export const dataCymbalsEns = [
     { menu: 'Comida', categorie: 'Ensaladas y sopas', name: 'Ensalada betabel', price: '220', description: 'Burrata,betabel,fresa,tomate,nuez y vinagreta de frambuesa' },
     { menu: 'Comida', categorie: 'Ensaladas y sopas', name: 'Ensalada verde', price: '160', description: 'Manzana,uva,pepino,piñon y vinagreta de limon con miel' },
     { menu: 'Comida', categorie: 'Ensaladas y sopas', name: 'Sopa de hongos', price: '180', description: 'Amaranto,guajillo y epazote' },
     { menu: 'Comida', categorie: 'Ensaladas y sopas', name: 'Bisque de camaron', price: '240', description: 'Sofrito de pimientos con chile pasilla mize' },
     { menu: 'Comida', categorie: 'Ensaladas y sopas', name: 'Crema de tomate', price: '170', description: 'Sazonada con chicharron de cerdo' },
-
-]
-export const dataCymbalsShare = [
     { menu: 'Comida', categorie: 'Para compartir', name: 'Tabla de quesos', price: '395', description: 'Quesos de la region, aceitunas,charcuteria y pan de la casa' },
     { menu: 'Comida', categorie: 'Para compartir', name: 'Pezcado Zarandeado', price: '1250', description: 'A las brazas, ensalada y vegetales' },
     { menu: 'Comida', categorie: 'Para compartir', name: 'Tomahawk', price: '2200', description: 'Aligot y vegetales' },
-
     { menu: 'Comida', categorie: 'Para compartir', name: 'Lengua de res', price: '440', description: 'Salsa verde para taquear' },
-
-
-
-]
-export const dataCymbalsBig = [
     { menu: 'Comida', categorie: 'Platos Fuertes', name: 'Pesca del dia ', price: '420', description: 'Gremolata de algas y emulsion de chiles' },
     { menu: 'Comida', categorie: 'Platos Fuertes', name: 'Pulpo adobado', price: '450', description: 'Adobado con achiote y miso,esferas de papa, pure de zanahoria' },
     { menu: 'Comida', categorie: 'Platos Fuertes', name: 'Cola de langosta', price: '720', description: 'Con risotto de salsa de langosta' },
@@ -83,49 +70,26 @@ export const dataCymbalsBig = [
     { menu: 'Comida', categorie: 'Platos Ffuertes', name: 'Lechon confitado', price: '425', description: 'Mole encacahuatado y vegetales en escabeche' },
     { menu: 'Comida', categorie: 'Platos Fuertes', name: 'Chamorro de borrego', price: '690', description: 'Braseado con pure de coliflor' },
     { menu: 'Comida', categorie: 'Platos Fuertes', name: 'New York', price: '890', description: 'Al grill con aligot y vegetales' },
-
-
-
-]
-export const dataCymbalsDesserts = [
     { menu: 'Comida', categorie: 'Postres', name: 'Selva negra', price: '220', description: 'Mouse de chocolate, cujriente de cacao y cumbre de brownie' },
     { menu: 'Comida', categorie: 'Postres', name: 'Limon real', price: '180', description: 'Limon confitado, ganache de limon  y cumbre de mantequilla' },
     { menu: 'Comida', categorie: 'Postres', name: 'Tarta rustica', price: '180', description: 'Galette de fruta de temporada' },
     { menu: 'Comida', categorie: 'Postres', name: 'Baklava', price: '180', description: 'Pasta fillo , nuez, canela y mantequilla' },
     { menu: 'Comida', categorie: 'Postres', name: 'Cafe lechero', price: '175', description: 'Panna cotta de leche y jarabe de cafe veracruzano' },
-
-
-]
-export const dataBreakfastStart = [
     { menu: 'Desayunos', categorie: 'Para despertar', name: 'Bowl de fruta con yogurt', price: '115', description: 'yogurt de frutas , frutas de temporada, granola y miel de abeja' },
     { menu: 'Desayunos', categorie: 'Para despertar', name: 'Plato de frutas con ricotta', price: '145', description: 'Queso ricotta, fruta de temporada y nuez caramelizada' },
     { menu: 'Desayunos', categorie: 'Para despertar', name: 'Avocado Toast', price: '125', description: 'Pan de masa madre, aguacate , tomate y huevo pochado' },
-]
-export const dataBreakfastShare = [
-    { menu: 'Desayunos', categorie: 'Compartir', name: 'Borrego tatemado', price: '270', description: 'Orden de borrego tatemado,frijoles,queso fresco y tortillas' },
+    { menu: 'Desayunos', categorie: 'Para Compartir', name: 'Borrego tatemado', price: '270', description: 'Orden de borrego tatemado,frijoles,queso fresco y tortillas' },
     { menu: 'Desayunos', categorie: 'Para despertar', name: 'Shakshuka', price: '200', description: 'Vegetales salteados en salsa de tomado,aceite de oliva y especias, huevos estrellados y queso de cabra ' },
-
-]
-
-export const dataBreakfastOmelets = [
     { menu: 'Desayunos', categorie: 'Omelets', name: 'A la mexicana', price: '270', description: 'Cebolla,tomate, chile california, queso y salsa de frijol' },
     { menu: 'Desayunos', categorie: 'Omelets', name: 'Hongos', price: '220', description: 'Bañado en salsa de quesos, champiñones confitados , esparragos y tomate' },
     { menu: 'Desayunos', categorie: 'Omelets', name: 'Camaron', price: '280', description: 'Sofrito de camaron con tocino y queso, ensalada y aguacate' },
     { menu: 'Desayunos', categorie: 'Omelets', name: 'Costilla', price: '240', description: 'Salsa roja , costilla de res braseada, queso y frijoles' },
-]
-export const dataBreakfastChila = [
     { menu: 'Desayunos', categorie: 'Chilaquiles', name: 'Rojos', price: '140', description: 'Salsa roja, queso cotija, cebolla y huevo al gusto con frijoles' },
     { menu: 'Desayunos', categorie: 'Chilaquiles', name: 'Hongos', price: '140', description: 'Salsa roja, queso cotija, cebolla y huevo al gusto con frijoles' },
     { menu: 'Desayunos', categorie: 'Chilaquiles', name: 'Camaron', price: '140', description: 'SSalsa roja, queso cotija, cebolla y huevo al gusto con frijoles' },
-
-]
-export const dataBreakfastSweet = [
     { menu: 'Desayunos', categorie: 'Lo Dulce', name: 'Wafles', price: '210', description: 'Salsa toffe, helado espresso y fruta' },
     { menu: 'Desayunos', categorie: 'Lo Dulce', name: 'Pan fancres', price: '110', description: 'Wafle , miel , almendra y fruta' },
     { menu: 'Desayunos', categorie: 'Lo Dulce', name: 'Canasta de pan', price: '110', description: 'Pan dulce hecho en casa' },
-
-]
-export const dataDrinks = [
     { menu: 'Desayunos', categorie: 'Bebidas Sin Alcohol', name: 'Piñada', price: '120', description: '' },
     { menu: 'Bebidas', categorie: 'Bebidas Sin Alcohol', name: 'Limonada natural', price: '45', description: '' },
     { menu: 'Bebidas', categorie: 'Bebidas Sin Alcohol', name: 'Limonada mineral', price: '45', description: '' },
@@ -134,39 +98,19 @@ export const dataDrinks = [
     { menu: 'Bebidas', categorie: 'Bebidas Sin Alcohol', name: 'Sprite', price: '120', description: '' },
     { menu: 'Bebidas', categorie: 'Bebidas Sin Alcohol', name: 'Piñada', price: '120', description: '' },
     { menu: 'Bebidas', categorie: 'Bebidas Sin Alcohol', name: 'Piñada', price: '120', description: '' },
-
-
-
-]
-export const dataCoffe = [
     { menu: 'Cafeteria', categorie: 'Cafe', name: 'Espresso', price: '50', description: '' },
     { menu: 'Cafeteria', categorie: 'Cafe', name: 'Cafe americano', price: '50', description: '' },
     { menu: 'Cafeteria', categorie: 'Cafe', name: 'Latte', price: '50', description: '' },
     { menu: 'Cafeteria', categorie: 'Cafe', name: 'Capuccino', price: '50', description: '' },
     { menu: 'Cafeteria', categorie: 'Cafe', name: 'Mocha', price: '50', description: '' },
-
-]
-export const dataTe = [
     { menu: 'Cafeteria', categorie: 'Té', name: 'Negro', price: '50', description: '' },
     { menu: 'Cafeteria', categorie: 'Té', name: 'Verde', price: '50', description: '' },
     { menu: 'Cafeteria', categorie: 'Té', name: 'Manzanilla', price: '50', description: '' },
-
     { menu: 'Desayunos', categorie: 'Té', name: 'Jazmin', price: '50', description: '' },
-
-
-
-]
-export const dataMixology = [
     { menu: 'Mixologia', categorie: 'Mixologia de la casa', name: 'Carajillo', price: '210', description: 'Licor 43, cerveza artesanal y espresso' },
     { menu: 'Mixologia', categorie: 'Mixologia de la casa', name: 'La chingadera', price: '210', description: 'Jarabe de gengibre, agua tonica y cocacola' },
-    { menu: 'Mixologia', categorie: 'Mixologia de la casa', name: 'La chingadera', price: '210', description: 'Jarabe de gengibre, agua tonica y cocacola' }, { menu: 'Mixologia', categorie: 'Mixologia de la casa', name: 'La chingadera', price: '210', description: 'Jarabe de gengibre, agua tonica y cocacola' }, { menu: 'Mixologia', categorie: 'Mixologia de la casa', name: 'La chingadera', price: '210', description: 'Jarabe de gengibre, agua tonica y cocacola' }, { menu: 'Mixologia', categorie: 'Mixologia de la casa', name: 'La chingadera', price: '210', description: 'Jarabe de gengibre, agua tonica y cocacola' }, { menu: 'Mixologia', categorie: 'Mixologia de la casa', name: 'La chingadera', price: '210', description: 'Jarabe de gengibre, agua tonica y cocacola' },
-
-
-
-
-
-]
-export const dataMixologyClasic = [
+    { menu: 'Mixologia', categorie: 'Mixologia de la casa', name: 'La chingadera', price: '210', description: 'Jarabe de gengibre, agua tonica y cocacola' }, 
+    { menu: 'Mixologia', categorie: 'Mixologia de la casa', name: 'La chingadera', price: '210', description: 'Jarabe de gengibre, agua tonica y cocacola' }, { menu: 'Mixologia', categorie: 'Mixologia de la casa', name: 'La chingadera', price: '210', description: 'Jarabe de gengibre, agua tonica y cocacola' }, { menu: 'Mixologia', categorie: 'Mixologia de la casa', name: 'La chingadera', price: '210', description: 'Jarabe de gengibre, agua tonica y cocacola' }, { menu: 'Mixologia', categorie: 'Mixologia de la casa', name: 'La chingadera', price: '210', description: 'Jarabe de gengibre, agua tonica y cocacola' },
     { menu: 'Mixologia', categorie: 'Coctelería Clásica', name: 'Abe', price: '190', description: '' },
     { menu: 'Mixologia', categorie: 'Coctelería Clásica', name: 'Abe', price: '190', description: '' },
     { menu: 'Mixologia', categorie: 'Coctelería Clásica', name: 'Abe', price: '190', description: '' },
@@ -176,10 +120,6 @@ export const dataMixologyClasic = [
     { menu: 'Mixologia', categorie: 'Coctelería Clásica', name: 'Abe', price: '190', description: '' },
     { menu: 'Mixologia', categorie: 'Coctelería Clásica', name: 'Abe', price: '190', description: '' },
     { menu: 'Mixologia', categorie: 'Coctelería Clásica', name: 'Abe', price: '190', description: '' },
-
-
-]
-export const dataMixologyLiqor = [
     { menu: 'Mixologia', categorie: 'Licores Fuertes', name: '1800', price: '2700', description: 'shot:160' },
     { menu: 'Mixologia', categorie: 'Licores Fuertes', name: '1800', price: '2700', description: 'shot:160' },
     { menu: 'Mixologia', categorie: 'Licores Fuertes', name: '1800', price: '2700', description: 'shot:160' },
@@ -192,36 +132,16 @@ export const dataMixologyLiqor = [
     { menu: 'Mixologia', categorie: 'Licores Fuertes', name: '1800', price: '2700', description: 'shot:160' },
     { menu: 'Mixologia', categorie: 'Licores Fuertes', name: '1800', price: '2700', description: 'shot:160' },
     { menu: 'Mixologia', categorie: 'Licores Fuertes', name: '1800', price: '2700', description: 'shot:160' },
-
-
-]
-export const dataVinoPremium = [
     { menu: 'Vino', categorie: 'Premium', name: 'Negro', price: '50', description: '' },
     { menu: 'Cafeteria', categorie: 'Té', name: 'Verde', price: '50', description: '' },
     { menu: 'Cafeteria', categorie: 'Té', name: 'Manzanilla', price: '50', description: '' },
-
     { menu: 'Desayunos', categorie: 'Té', name: 'Jazmin', price: '50', description: '' },
-
-
-
-]
-export const dataVinoReserva = [
     { menu: 'Vino', categorie: 'Premium', name: 'Negro', price: '50', description: '' },
     { menu: 'Cafeteria', categorie: 'Té', name: 'Verde', price: '50', description: '' },
     { menu: 'Cafeteria', categorie: 'Té', name: 'Manzanilla', price: '50', description: '' },
-
     { menu: 'Desayunos', categorie: 'Té', name: 'Jazmin', price: '50', description: '' },
-
-
-
-]
-export const dataVinoWhite = [
     { menu: 'Vino', categorie: 'Premium', name: 'Negro', price: '50', description: '' },
     { menu: 'Cafeteria', categorie: 'Té', name: 'Verde', price: '50', description: '' },
     { menu: 'Cafeteria', categorie: 'Té', name: 'Manzanilla', price: '50', description: '' },
-
     { menu: 'Desayunos', categorie: 'Té', name: 'Jazmin', price: '50', description: '' },
-
-
-
 ]
