d99206456b7e7c2ee8f697d4f4093423fd5732f1
itsaimejia <33639089+itsaimejia@users.noreply.github.com>
2022-11-21 17:10:46 -0800

    correccion error id categorie

--- pages/api/menus.ts
+++ pages/api/menus.ts
@@ -1,14 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next"
 
-
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<any>
 ) {
-    const url = 'https://almatierra-7796b-default-rtdb.firebaseio.com/dataCymbals.json'
+    const url = 'https://almatierra-7796b-default-rtdb.firebaseio.com/menus.json'
     const r = await fetch(url)
     let json = await r.json()
-    let cymbals = json.filter((e: any) => e !== null)
-    res.status(200).json(cymbals)
+    let menus = json.filter((e: any) => e !== null)
+    res.status(200).json(menus)
 }
-
